fd9bd5bc53a2b34a50c5ff4ba8f48498f765fb6a
Neeraj Kumar <neeraj.k@qapitol.com>
2018-12-09 17:14:59 +0530

    Set Example Added

---
+++ Set.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+int main()
+{
+  set <int> s;
+  set <int>::iterator it;
+  int A[] = {3, 5, 2, 1, 5, 4};
+  for(int i=0;i<6;i++)
+     s.insert(A[i]);
+  for(it = s.begin(); it !=s.end(); i++)
+     cout << *it <<' ';
+  cout << endl;
+   return 0;
+ }
